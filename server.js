eed6d11698cda9762116ef0763a1c2df6cdfe523
nao_iijima <nao_iijima@worksid.co.jp>
2025-09-09 16:25:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -84,10 +84,11 @@ app.post('/', async (req, res) => {
 });
 
 // --- サーバーの起動 ---
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Final kintone AI server is running on port ${PORT}`);
 });
+
 
 
 
